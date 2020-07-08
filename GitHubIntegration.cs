444bdecb7629438dc987ca83456ecfb411436c65
Rafael Amorim <contatofael@gmail.com>
2020-07-08 19:54:04 +0100

    removing get method

--- GitHubIntegration.cs
+++ GitHubIntegration.cs
@@ -14,7 +14,7 @@ namespace Demo.AzFunction
     {
         [FunctionName("GitHubIntegration")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function,  "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
